04f990b2f4b63528709e8ab80cf6c5038974957d
yongseungLeeDec <yonson.dev@gmail.com>
2023-01-03 16:15:19 +0900

    solution: 2023-01-03 알고리듬 문제 풀이 (3개)

---
+++ Programmers/January_2023/Java/src/solution/Jan_03.java
@@ -0,0 +1,102 @@
+package solution;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class Jan_03 {
+
+    /*
+    문제 이름: 가장 가까운 같은 글자
+    문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/142086
+     */
+    public int[] solution_problem1(String s) {
+        int stringLength = s.length();
+        int[] answer = new int[stringLength];
+
+        int[] lastAppearedIndexes = new int[26];
+        Arrays.fill(lastAppearedIndexes, Integer.MIN_VALUE);
+
+        for (int i = 0; i < stringLength; ++i) {
+            char currentChar = s.charAt(i);
+            int alphabetIndex = (int) currentChar - 'a';
+
+            if (lastAppearedIndexes[alphabetIndex] == Integer.MIN_VALUE) {
+                answer[i] = -1;
+            } else {
+                answer[i] = i - lastAppearedIndexes[alphabetIndex];
+            }
+
+            lastAppearedIndexes[alphabetIndex] = i;
+        }
+
+        return answer;
+    }
+
+    /*
+    문제 이름: 문자열 나누기
+    문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/140108
+    */
+    public int solution_problem2(String s) {
+        int strLen = s.length();
+        int numTokens = 0;
+        char firstChar = s.charAt(0);
+        int sameCharCounts = 1;
+        int diffCharCounts = 0;
+
+        boolean startAgain = false;
+
+        for (int i = 1; i < strLen; ++i) {
+            if (startAgain) {
+                firstChar = s.charAt(i);
+                sameCharCounts = 1;
+                diffCharCounts = 0;
+                startAgain = false;
+                continue;
+            }
+
+            if (s.charAt(i) == firstChar) {
+                sameCharCounts++;
+            } else {
+                diffCharCounts++;
+            }
+
+            if (sameCharCounts == diffCharCounts) {
+                numTokens++;
+                startAgain = true;
+            }
+        }
+
+        if (!startAgain) {
+            numTokens++;
+        }
+
+        return numTokens;
+    }
+
+    /*
+    문제 이름: 명예의 전당 (1)
+    문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/138477
+     */
+    public int[] solution_problem3(int k, int[] score) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(k);
+        int[] dailyMinScores = new int[score.length];
+
+        int count = 0;
+
+        while (count < k && count < score.length) {
+            pq.add(score[count]);
+            dailyMinScores[count] = pq.peek();
+            count++;
+        }
+
+        for (int i = k; i < score.length; i++) {
+            if (pq.peek() < score[i]) {
+                pq.poll();
+                pq.add(score[i]);
+            }
+            dailyMinScores[i] = pq.peek();
+        }
+
+        return dailyMinScores;
+    }
+}
