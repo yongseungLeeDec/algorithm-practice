72ada249d6e83d5190d7e819d91b6df39f21aff2
yongseungLeeDec <yonson.dev@gmail.com>
2023-01-17 17:59:16 +0900

    solution: today is too rough for me..

---
+++ Programmers/January_2023/Java/src/solution/Jan_17.java
@@ -0,0 +1,9 @@
+package solution;
+
+public class Jan_17 {
+    public static final int THIS_YEAR = 2022;
+
+    public int solution_problem1(int age) {
+        return THIS_YEAR - age + 1;
+    }
+}
