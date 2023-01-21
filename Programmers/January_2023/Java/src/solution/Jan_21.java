4ed79776a56294a902a5f8d6246c9d6efeeeadf7
yongseungLeeDec <yonson.dev@gmail.com>
2023-01-21 18:07:04 +0900

    solution: add a solution of January 21st

--- Programmers/January_2023/Java/src/solution/Jan_21.java
+++ Programmers/January_2023/Java/src/solution/Jan_21.java
@@ -1,5 +1,7 @@
 package solution;
 
+import java.util.Arrays;
+
 public class Jan_21 {
     public int solution(int n) {
         if (n % 2 == 1) {
@@ -17,4 +19,37 @@ public class Jan_21 {
 
         return cache[n];
     }
+
+    /*
+     유클리드 호제법을 한 번은 공부해서 이해하자.
+
+     */
+    public int solution(int[] arr) {
+        Arrays.sort(arr);
+        int lcm = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            lcm = getLeastCommonMultiple(lcm, arr[i]);
+        }
+        return lcm;
+    }
+
+    private int getLeastCommonMultiple(int n, int m) {
+        return (n * m) / getGreatestCommonDivisor(n, m);
+    }
+
+    private int getGreatestCommonDivisor(int n, int m) {
+        int a = Math.max(n, m);
+        int b = (a == n) ? m : n;
+        int r = a % b;
+
+        while (b != 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+
+        return a;
+    }
+
+
 }
