e8ad4da4371c7d0ef5262a7b9a49dcb55735806d
yongseungLeeDec <yonson.dev@gmail.com>
2023-01-29 15:06:18 +0900

    solution: add a solution (not optimized)
    https://school.programmers.co.kr/learn/courses/30/lessons/12978

--- Programmers/January_2023/Java/src/solution/Jan_29.java
+++ Programmers/January_2023/Java/src/solution/Jan_29.java
@@ -5,14 +5,14 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class Jan_29 {
-    // 어디서 루프가 생기지?
+    // 어디서 루프가 생기지? // 루프가 아니라 개수 문제일 ㅜㅅ도..
     public static void main(String[] args) {
         int[][] road = new int[][]{{1, 2, 1}, {1, 3, 2}, {2, 3, 2}, {3, 4, 3}, {3, 5, 2}, {3, 5, 3}, {5, 6, 1}};
         System.out.println(solution(6, road, 4));
     }
 
     public static int solution(int N, int[][] road, int K) {
-        HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> map = getMap(road);
+        HashMap<Integer, HashMap<Integer, Integer>> map = getMap(road);
         HashSet<Integer> traversedTowns = new HashSet<>();
         HashSet<Integer> reachableTowns = new HashSet<>();
 
@@ -22,11 +22,11 @@ public class Jan_29 {
     }
 
     private static void travelMap(Integer currentTown,
-                           int takenHours,
-                           int K,
-                           HashSet<Integer> traversedTowns,
-                           HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> map,
-                           HashSet<Integer> reachableTowns) {
+                                  int takenHours,
+                                  int K,
+                                  HashSet<Integer> traversedTowns,
+                                  HashMap<Integer, HashMap<Integer, Integer>> map,
+                                  HashSet<Integer> reachableTowns) {
 
         if (takenHours > K) {
             return;
@@ -35,20 +35,18 @@ public class Jan_29 {
         reachableTowns.add(currentTown);
         traversedTowns.add(currentTown);
 
-        HashMap<Integer, ArrayList<Integer>> connectedTowns = map.get(currentTown);
-        for (Integer town : connectedTowns.keySet()) {
-            ArrayList<Integer> roads = connectedTowns.get(town);
+        HashMap<Integer, Integer> connectedTowns = map.get(currentTown);
 
-            for (Integer road : roads) {
-                travelMap(town, takenHours + road, K, traversedTowns, map, reachableTowns);
-            }
+        for (Integer town : connectedTowns.keySet()) {
+            int dist = connectedTowns.get(town);
+            travelMap(town, takenHours + dist, K, traversedTowns, map, reachableTowns);
         }
 
         traversedTowns.remove(currentTown);
     }
 
-    private static HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> getMap(int[][] road) {
-        HashMap<Integer, HashMap<Integer, ArrayList<Integer>>> map = new HashMap<>();
+    private static HashMap<Integer, HashMap<Integer, Integer>> getMap(int[][] road) {
+        HashMap<Integer, HashMap<Integer, Integer>> map = new HashMap<>();
 
         for (int[] r : road) {
             int sour = r[0];
@@ -58,11 +56,16 @@ public class Jan_29 {
             map.computeIfAbsent(sour, k -> new HashMap<>());
             map.computeIfAbsent(dest, k -> new HashMap<>());
 
-            map.get(sour).computeIfAbsent(dest, k -> new ArrayList<>());
-            map.get(dest).computeIfAbsent(sour, k -> new ArrayList<>());
+            map.get(sour).putIfAbsent(dest, Integer.MAX_VALUE);
+            map.get(dest).putIfAbsent(sour, Integer.MAX_VALUE);
 
-            map.get(sour).get(dest).add(dist);
-            map.get(dest).get(sour).add(dist);
+            int currDist = map.get(sour).get(dest);
+            int renewedDist = Math.min(currDist, dist);
+
+            if (renewedDist < currDist) {
+                map.get(sour).put(dest, renewedDist);
+                map.get(dest).put(sour, renewedDist);
+            }
         }
 
         return map;
