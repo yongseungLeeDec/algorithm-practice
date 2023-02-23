8d186c41bda93ed2f121c50d6f07c13a6b839280
yongseungLeeDec <yonson.dev@gmail.com>
2023-02-23 20:40:46 +0900

    solution: add a solution (k진수에서 소수 개수 구하기)
    생각보다는 싱거운 문제
    
    https://school.programmers.co.kr/learn/courses/30/lessons/92335

--- Programmers/February_2023/src/solution/Feb_23.java
+++ Programmers/February_2023/src/solution/Feb_23.java
@@ -3,6 +3,9 @@ package solution;
 import java.util.PriorityQueue;
 
 public class Feb_23 {
+    public static void main(String[] args) {
+        System.out.println(getTargetNumberString(54, 3));
+    }
     public int solution(int n, int k, int[] enemy) {
         PriorityQueue<Integer> queue = new PriorityQueue<>();
 
@@ -19,5 +22,53 @@ public class Feb_23 {
         }
 
         return enemy.length;
+    }
+
+    public int solution(int n, int k) {
+        String number = getTargetNumberString(n, k);
+        String[] nums = number.split("0+");
+
+        int primeCount = 0;
+
+        for (String num : nums) {
+            if (isPrime(Long.parseLong(num))) {
+                primeCount++;
+            }
+        }
+
+        return primeCount;
+    }
+
+    private boolean isPrime(long num) {
+        if (num == 1) {
+            return false;
+        }
+
+        int sqrt = (int) Math.sqrt(num);
+
+        int div = 2;
+        while (div <= sqrt) {
+            if (num % div == 0) {
+                return false;
+            }
+            div++;
+        }
+
+        return true;
+    }
+
+    private static String getTargetNumberString(int n, int k) {
+        StringBuilder nBaseK = new StringBuilder();
+
+        while (n > 1) {
+            nBaseK.append(n % k);
+            n = (n / k);
+        }
+
+        if (n != 0) {
+            nBaseK.append(n);
+        }
+
+        return nBaseK.reverse().toString();
     }
 }
